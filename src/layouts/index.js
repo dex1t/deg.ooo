f9771630273b940f33e9b70ae5ccf11d4d3fd828
dex1t <dex1t@deg.ooo>
2018-05-20 20:45:19 +0900

    Tweak

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,21 +10,14 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        {
+          name: 'description',
+          content:
+            'Takaya Deguchi (dex1t) is UX Engineer & Product Manager in Tokyo.',
+        },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <main>{children()}</main>
   </div>
 )
 
